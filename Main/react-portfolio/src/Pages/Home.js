ab0a22e9cc6b527e54585e9e2f8114f8dba645d2
unknown <charliegarvin90@gmail.com>
2023-05-29 17:02:25 -0600

    finished the home page and am now importing project data and pictures

--- Main/react-portfolio/src/Pages/Home.js
+++ Main/react-portfolio/src/Pages/Home.js
@@ -24,15 +24,17 @@ function Home() {
             <ol className="list">
                 <li className="item">
                     <h2>Front-End</h2>
-                    <span>enter front end info</span>
+                    <span>I am a new FullStack Developer who has recently finished a bootcamp with the University of Utah. I have spent a fair amount of time working with Front-End
+                        languages like HTML, CSS, and JS. I am looking for opportunities to increase my abilities and learn new skills in the field.
+                    </span>
                 </li>
                 <li className="item">
                     <h2>Back-End</h2>
-                    <span>enter back end info</span>
+                    <span>The bootcamp I went through also taught of lots of back-end skills and languages some of those for example are: MySQL, NoSQL, JWT, and more.</span>
                 </li>
                 <li className="item">
                     <h2>Languages</h2>
-                    <span> HTML, CSS, JS, Bootstrap, React</span>
+                    <span> HTML, CSS, JS, Bootstrap, React, MySQL, NoSQL</span>
                 </li>
             </ol>
         </div>
--- Main/react-portfolio/src/helpers/ProjectList.js
+++ Main/react-portfolio/src/helpers/ProjectList.js
@@ -1,9 +1,21 @@
-// import Proj1 from "../assests/name"
+import Proj1 from "../assests/uniquist.jpg";
+import Proj2 from "../assests/typeninja.png";
+import Proj3 from "../assests/MixStreamDemo.png";
 
 export const ProjectList = [
     {
-        name: "Project name",
-        // image: proj1
+        name: "Uniquist",
+        image: Proj1,
+        skills: "list of skills"
+    },
+    {
+        name: "Type Ninja",
+        image: Proj2,
+        skills: "list of skills"
+    },
+    {
+        name: "MixStream",
+        image: Proj3,
         skills: "list of skills"
     }
 ];
