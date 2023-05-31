4935312c08a6110410b0ad8251baead547597e7c
unknown <charliegarvin90@gmail.com>
2023-05-31 16:49:03 -0600

    trying to debug having my profile picture appear

--- Main/react-portfolio/src/components/Footer.js
+++ Main/react-portfolio/src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from "react";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+// import "../assets/portfolio-pic.JPG";
 import "../styles/Footer.css";
 
 function Footer() {
     return( <div className="footer">
     <div className="socialMedia"> <LinkedInIcon /> </div>
+    <img id="logo" src="../assets/portfolio-pic.JPG" alt="picture of me"/>
     <p> &copy; 2023</p>
     </div>
   );
+  
 }
 
 export default Footer;
