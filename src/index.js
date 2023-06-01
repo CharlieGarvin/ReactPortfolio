61f7c18948550125e5a943f0e309932c1f923882
unknown <charliegarvin90@gmail.com>
2023-06-01 15:03:02 -0600

    bug fixes in index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const root = ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById(`root`)
+  document.getElementById(root)
 );
 
 // If you want to start measuring performance in your app, pass a function
